f99e3b9c56ec0cee5cfe69697048fa00e558a519
illinai <144272615+illinai@users.noreply.github.com>
2025-03-25 05:10:53 -0700

    coin script

--- Assets/scripts/Coins.cs
+++ Assets/scripts/Coins.cs
@@ -2,15 +2,17 @@ using UnityEngine;
 
 public class Coins : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public GameManager gm;
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            //GameManager.instance.AddScore(1); // Add to score
+            gm.incrementScore();
+            Destroy(gameObject); // Remove coin
+        }
+
     }
 }
--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -2,15 +2,11 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private int score = 0;
 
-    // Update is called once per frame
-    void Update()
+    public void incrementScore()
     {
-        
+        score++;
+     
     }
 }
