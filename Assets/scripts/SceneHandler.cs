12675ddda4ec6912e76acd61431425e958584eae
Joy Musiel-Henseleit <joymusiel@gmail.com>
2025-03-28 23:06:34 -0700

    Add button hooks and game over scene

--- Assets/scripts/SceneHandler.cs
+++ Assets/scripts/SceneHandler.cs
@@ -10,29 +10,24 @@ public class SceneHandler : SingletonMonoBehavior<SceneHandler>
     [Header("Scene Data")]
     [SerializeField] private List<string> levels;
     [SerializeField] private string menuScene;
+    [SerializeField] private string gameOverScene;
     
     [Header("Transition Animation Data")]
     [SerializeField] private Ease menuAnimationType;
-    [SerializeField] private Ease gameOverAnimationType;
     [SerializeField] private float animationDuration;
-    [SerializeField] private float gameOverAnimationDuration;
     [SerializeField] private RectTransform transitionCanvas;
-    [SerializeField] private RectTransform gameOverCanvas;
     private int nextLevelIndex;
-    private float initXPosition;
     private float initYPosition;
     protected override void Awake()
     {
         base.Awake();
         initYPosition = transitionCanvas.transform.localPosition.y;
-        initXPosition = gameOverCanvas.transform.localPosition.x;
         SceneManager.LoadScene(menuScene);
         SceneManager.sceneLoaded += OnSceneLoad;
     }
 
     private void OnSceneLoad(Scene scene, LoadSceneMode _)
     {
-        gameOverCanvas.DOLocalMoveX(initXPosition, gameOverAnimationDuration).SetEase(gameOverAnimationType);
         transitionCanvas.DOLocalMoveY(initYPosition, animationDuration).SetEase(menuAnimationType);
     }
 
@@ -59,7 +54,9 @@ public class SceneHandler : SingletonMonoBehavior<SceneHandler>
 
     public void LoadGameOverScene()
     {
-        gameOverCanvas.DOLocalMoveX(initXPosition + gameOverCanvas.rect.width, gameOverAnimationDuration).SetEase(gameOverAnimationType);
+        transitionCanvas.DOLocalMoveY(initYPosition - transitionCanvas.rect.height, animationDuration).SetEase(menuAnimationType);
+        StartCoroutine(LoadSceneAfterTransition(gameOverScene));
+        nextLevelIndex = 0;
     }
 
     private IEnumerator LoadSceneAfterTransition(string scene)
