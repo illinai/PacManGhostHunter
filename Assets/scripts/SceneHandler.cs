45509e838b3bf3b32e0ca2bd1780e4af115f1fc2
Joy Musiel-Henseleit <joymusiel@gmail.com>
2025-03-28 18:38:13 -0700

    Add scene handler script

--- Assets/scripts/SceneHandler.cs
+++ Assets/scripts/SceneHandler.cs
@@ -1,20 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
-
+using DG.Tweening;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneHandler : MonoBehaviour
+public class SceneHandler : SingletonMonoBehavior<SceneHandler>
 {
-
-    void Start()
+    [Header("Scene Data")]
+    [SerializeField] private List<string> levels;
+    [SerializeField] private string menuScene;
+    
+    [Header("Transition Animation Data")]
+    [SerializeField] private Ease menuAnimationType;
+    [SerializeField] private Ease gameOverAnimationType;
+    [SerializeField] private float animationDuration;
+    [SerializeField] private float gameOverAnimationDuration;
+    [SerializeField] private RectTransform transitionCanvas;
+    [SerializeField] private RectTransform gameOverCanvas;
+    private int nextLevelIndex;
+    private float initXPosition;
+    private float initYPosition;
+    protected override void Awake()
     {
-        
+        base.Awake();
+        initYPosition = transitionCanvas.transform.localPosition.y;
+        initXPosition = gameOverCanvas.transform.localPosition.x;
+        SceneManager.LoadScene(menuScene);
+        SceneManager.sceneLoaded += OnSceneLoad;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnSceneLoad(Scene scene, LoadSceneMode _)
     {
-        
+        gameOverCanvas.DOLocalMoveX(initXPosition, gameOverAnimationDuration).SetEase(gameOverAnimationType);
+        transitionCanvas.DOLocalMoveY(initYPosition, animationDuration).SetEase(menuAnimationType);
     }
+
+    public void LoadNextScene()
+    {
+        if(nextLevelIndex >= levels.Count)
+        {
+            LoadMenuScene();
+        }
+        else
+        {
+            transitionCanvas.DOLocalMoveY(initYPosition - transitionCanvas.rect.height, animationDuration).SetEase(menuAnimationType);
+            StartCoroutine(LoadSceneAfterTransition(levels[nextLevelIndex]));
+            nextLevelIndex++;
+        }
+    }
+
+    public void LoadMenuScene()
+    {
+        transitionCanvas.DOLocalMoveY(initYPosition - transitionCanvas.rect.height, animationDuration).SetEase(menuAnimationType);
+        StartCoroutine(LoadSceneAfterTransition(menuScene));
+        nextLevelIndex = 0;
+    }
+
+    public void LoadGameOverScene()
+    {
+        gameOverCanvas.DOLocalMoveX(initXPosition + gameOverCanvas.rect.width, gameOverAnimationDuration).SetEase(gameOverAnimationType);
+    }
+
+    private IEnumerator LoadSceneAfterTransition(string scene)
+    {
+        yield return new WaitForSeconds(animationDuration);
+        SceneManager.LoadScene(scene);
+    }
+
 }
